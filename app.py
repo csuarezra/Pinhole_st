9591941e073b4527fb2512f7edd1cc232c385863
CuauSuarez <cuaudsr@gmail.com>
2023-07-21 10:50:45 -0600

    Add tables

--- app.py
+++ app.py
@@ -10,23 +10,69 @@ from pathlib import Path
 from matplotlib import pyplot as plt
 from PIL import Image
 
-col1, col2 = st.columns(2)
-
-
-
-st.markdown("<h1 style='text-align: center;'>Pinhole Depth Detector</h1>", unsafe_allow_html=True)
-st.markdown("<h2 style='text-align: center;'>Pinhole Depth Detector></h2>", unsafe_allow_html=True)
-
-images = st.file_uploader("Drop your images", accept_multiple_files=True)
+image = Image.open('images/vitro_logo.png')
 img_path = "img.jpg"
 
-for image in images:
-    # Saves
-    img = Image.open(image)
-    img = img.save("img.jpg")
+def process_images(images):
+    results = []
 
-    depth = imp.complete_depth(img_path)
-    print(depth)
+    for image in images:
+        # Saves
+        img = Image.open(image)
+        img = img.save("img.jpg")
 
-if os.path.exists(img_path):
-    os.remove(img_path)
+        depth = imp.complete_depth(img_path)
+        results.append({'image': os.path.splitext(image.name)[0], 'depth': depth})
+
+    if os.path.exists(img_path):
+        os.remove(img_path)
+
+    df = pd.DataFrame(results)
+    return df    
+
+
+col1, col2 = st.columns([0.3, 0.7])
+with col1:
+    st.image(image, width=200)
+with col2:
+    #st.title("VitroGPT")
+    st.markdown("<h1 style='text-align: center;'>Pinhole Depth Detector</h1>", unsafe_allow_html=True)
+
+
+#st.markdown("<h1 style='text-align: center;'>Pinhole Depth Detector</h1>", unsafe_allow_html=True)
+#st.markdown("<h2 style='text-align: center;'>Pinhole Depth Detector></h2>", unsafe_allow_html=True)
+st.markdown("---")
+
+images = st.file_uploader("Drop your images", accept_multiple_files=True, type=["jpg", "png", "jpeg"])
+
+col1, col2, col3 , col4, col5 = st.columns(5)
+
+with col1:
+    pass
+with col2:
+    pass
+with col4:
+    pass
+with col5:
+    pass
+with col3 :
+    proc_btn = col3.button("Process")
+
+
+st.markdown("---")
+
+block = st.empty()
+
+if proc_btn:
+    if not images:
+        st.warning("Please, upload some images.")
+    else:
+        table = process_images(images)
+        block.table(table)
+        st.download_button(
+            label="Download data as CSV",
+            data=table.to_csv().encode('utf-8'),
+            file_name='depths.csv',
+            mime='text/csv'
+        )
+        
